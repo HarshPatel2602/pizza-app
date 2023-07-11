a17eebe59fdcfe6396bce70ea7abc7ed82f9b9fd
HarshPatel2602 <harshpatidar2602@gmail.com>
2023-07-11 19:26:31 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import InstagramIcon  from '@mui/icons-material/Instagram'
-import TwitterIcon  from '@mui/icons-material/Twitter'
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from 'react';
+import Instagram from '../assets/instagram.png'
+import Facebook from '../assets/facebook.png'
+import Twitter from '../assets/twitter.png'
+import Linkedin from '../assets/linkedin.png'
 import '../styles/Footer.css'
 
 function Footer() {
   return (
     <div className='footer'>
       <div className='sociakMedia'>
-      <InstagramIcon />
-      <TwitterIcon />
-      <FacebookIcon />
-      <LinkedInIcon />
+      <img className='Twitter' src={Twitter} />
+      <img className='Instagram' src={Instagram} />
+      <img className='Facebook' src={Facebook} />
+      <img className='Linkedin' src={Linkedin} />
       </div>
-      <p>&copy; 2023 pedrotechpizza.com</p>
+      <p>&copy; 2023 harshpizza.com</p>
     </div>
   )
 }
-
 export default Footer
